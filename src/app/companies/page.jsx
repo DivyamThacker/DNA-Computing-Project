e95658a9b49216dbc9f14ec88381a33cc95635f8
Divyam <divyamthacker4147@gmail.com>
2023-11-01 00:34:08 +0530

    debug vercel error

--- src/app/companies/page.jsx
+++ src/app/companies/page.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import data from "@data/companies";
 import styles from "./page.module.css";
-import CompanyCard from "../../components/CompanyCard";
 
 const CompaniesPage = () => {
   return (
     <div>
       <h1 className="ml-10"> Companies Working on DNA Computing </h1>
       <div className={`${styles.container}`}>
-        {data.map((item,idx)=>{
-          return (
-              <CompanyCard 
-                key = {idx}
-                idx = {idx}
-                path = {item.path}
-                description={item.Description}
-                organization={item.Organization}
-                type={item.Type}
-              />
-          )
-        })}
+      Hello
       </div>
     </div>
   );
